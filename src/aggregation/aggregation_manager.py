558566928514778517ed58ca2d77ad572f1fc0de
RobuDan <marian-daniel.robu@outlook.com>
2026-04-26 13:31:30 +0300

    Fixing Naming

--- src/aggregation/aggregation_manager.py
+++ src/aggregation/aggregation_manager.py
@@ -4,7 +4,7 @@ import logging
 import importlib
 
 from .time_manager import TimeManager
-from .acoustic_aggregator.incertitude_calculator import IncertitudeCalculator
+from .acoustic_aggregator.uncertainty_calculator import UncertaintyCalculator
 
 class AggregationManager:
     def __init__(self, audio_config, connection_pool, weather_enabled=False):
@@ -51,7 +51,7 @@ class AggregationManager:
                     self.aggregators.append(aggregator)
 
                     if param == "LAeq":
-                        incert_calc = IncertitudeCalculator(param, self.connection_pool, self.time_manager, self.weather_enabled)
+                        incert_calc = UncertaintyCalculator(param, self.connection_pool, self.time_manager, self.weather_enabled)
                         self.aggregators.append(incert_calc)
 
                         
